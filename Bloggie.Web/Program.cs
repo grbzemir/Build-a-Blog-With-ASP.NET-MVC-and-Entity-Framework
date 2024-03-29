8ee698b46afc8285c67f12e75770c92f373e1e31
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-03-29 23:17:54 +0300

    Second Commit

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Sql server baðlantýsýný kurup database girmek için!
 builder.Services.AddDbContext<BloggieDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("BloggiePortal")));
 
